6c86b65b18cbc3a7041cf37b3b7f13fb0a574737
Olivier Elamine <ddabdul@zoho.eu>
2025-04-28 14:06:15 +0500

    Created the CashFlowEntity and adjusted BondEntity

--- BondEntity+CoreDataProperties.swift
+++ BondEntity+CoreDataProperties.swift
@@ -1,14 +1,11 @@
-//
 //  BondEntity+CoreDataProperties.swift
 //  MyBondManager
 //
-//  Created by Olivier on 25/04/2025.
-//
+//  Created by Olivier on 28/04/2025.
 //
 
 import Foundation
 import CoreData
-
 
 extension BondEntity {
 
@@ -16,21 +13,43 @@ extension BondEntity {
         return NSFetchRequest<BondEntity>(entityName: "BondEntity")
     }
 
-    @NSManaged public var id: UUID
-    @NSManaged public var name: String
-    @NSManaged public var issuer: String
-    @NSManaged public var isin: String
-    @NSManaged public var wkn: String
-    @NSManaged public var parValue: Double
-    @NSManaged public var initialPrice: Double
+    @NSManaged public var acquisitionDate: Date
     @NSManaged public var couponRate: Double
     @NSManaged public var depotBank: String
-    @NSManaged public var acquisitionDate: Date
-    @NSManaged public var yieldToMaturity: Double
+    @NSManaged public var id: UUID
+    @NSManaged public var initialPrice: Double
+    @NSManaged public var isin: String
+    @NSManaged public var issuer: String
     @NSManaged public var maturityDate: Date
+    @NSManaged public var name: String
+    @NSManaged public var parValue: Double
+    @NSManaged public var wkn: String
+    @NSManaged public var yieldToMaturity: Double
 
+    // Use Set<> instead of NSSet
+    @NSManaged public var cashFlows: Set<CashFlowEntity>?
 }
 
-extension BondEntity : Identifiable {
+// MARK: - Convenience Accessors
 
+extension BondEntity {
+    /// Sorted array of cash flows
+    public var cashFlowsArray: [CashFlowEntity] {
+        (cashFlows ?? []).sorted { $0.date < $1.date }
+    }
+
+    @objc(addCashFlowsObject:)
+    @NSManaged public func addToCashFlows(_ value: CashFlowEntity)
+
+    @objc(removeCashFlowsObject:)
+    @NSManaged public func removeFromCashFlows(_ value: CashFlowEntity)
+
+    @objc(addCashFlows:)
+    @NSManaged public func addToCashFlows(_ values: Set<CashFlowEntity>)
+
+    @objc(removeCashFlows:)
+    @NSManaged public func removeFromCashFlows(_ values: Set<CashFlowEntity>)
 }
+
+extension BondEntity : Identifiable {}
+
---
+++ CashFlowEntity+CoreDataProperties.swift
@@ -0,0 +1,37 @@
+//  CashFlowEntity+CoreDataProperties.swift
+//  MyBondManager
+//
+//  Created by Olivier on 28/04/2025.
+//
+
+import Foundation
+import CoreData
+
+extension CashFlowEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CashFlowEntity> {
+        return NSFetchRequest<CashFlowEntity>(entityName: "CashFlowEntity")
+    }
+
+    // MARK: - Stored Properties (must match your .xcdatamodeld exactly)
+    @NSManaged public var date: Date
+    @NSManaged public var amount: Double
+    @NSManaged public var natureRaw: String
+    @NSManaged public var bond: BondEntity?
+
+    // MARK: - Typed Nature Enum
+    public enum Nature: String, CaseIterable {
+        case interest
+        case principal
+        case capitalGains
+    }
+
+    /// A Swift-native wrapper around the raw string.
+    public var nature: Nature {
+        get { Nature(rawValue: natureRaw) ?? .interest }
+        set { natureRaw = newValue.rawValue }
+    }
+}
+
+extension CashFlowEntity: Identifiable {}
+
